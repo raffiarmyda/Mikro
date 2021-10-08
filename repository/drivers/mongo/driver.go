7c90bf1157e89fec1d74885a676a58bc41364180
raffi <raffiarmyda1@gmail.com>
2021-10-08 09:38:47 +0700

    configure mongodb

--- repository/drivers/mongo/driver.go
+++ repository/drivers/mongo/driver.go
@@ -16,11 +16,11 @@ type ConfigDb struct {
 }
 
 func (config *ConfigDb) InitDb() *mongo.Client {
-	//uri := fmt.Sprintf("mongodb+srv://%v:%v@%v",
-	//	config.Username,
-	//	config.Password,
-	//	config.Cluster)
-	uri := "mongodb://localhost:27017"
+	uri := fmt.Sprintf("mongodb+srv://%v:%v@%v",
+		config.Username,
+		config.Password,
+		config.Cluster)
+	//uri := "mongodb://localhost:27017"
 
 	clientOptions := options.Client().ApplyURI(uri)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
