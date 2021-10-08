946f0f0df7d69085c90bdd347f0145acd3a65a15
raffi <raffiarmyda1@gmail.com>
2021-10-08 08:59:14 +0700

    finishing & mocks

--- app/routes/route.go
+++ app/routes/route.go
@@ -4,14 +4,18 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"mikro/app/middlewares"
+	"mikro/deliveries/products"
+	"mikro/deliveries/transactions"
 	"mikro/deliveries/users"
 )
 
 type ControllerList struct {
-	LoggerMiddleware   middlewares.MongoConfig
-	JWTMiddleware      middleware.JWTConfig
-	JWTMiddlewareAdmin middleware.JWTConfig
-	UserController     users.Controller
+	LoggerMiddleware      middlewares.MongoConfig
+	JWTMiddleware         middleware.JWTConfig
+	JWTMiddlewareAdmin    middleware.JWTConfig
+	UserController        users.Controller
+	ProductController     products.Controller
+	TransactionController transactions.Controller
 }
 
 func (cl *ControllerList) RouteUsers(e *echo.Echo) {
@@ -27,4 +31,16 @@ func (cl *ControllerList) RouteUsers(e *echo.Echo) {
 	v1.POST("users", cl.UserController.CreateUserController)
 	v1.DELETE("users/:userId", cl.UserController.DeleteUserController)
 	v1.PUT("users/:userId", cl.UserController.UpdateUserController)
+
+	v1.GET("products", cl.ProductController.GetProductsController)
+	v1.GET("products/:productId", cl.ProductController.GetDetailProductController)
+	v1.POST("products", cl.ProductController.CreateProductController)
+	v1.DELETE("products/:productId", cl.ProductController.DeleteProductController)
+	v1.PUT("products/:productId", cl.ProductController.UpdateProductController)
+
+	v1.GET("transactions", cl.TransactionController.GetTransactionsController)
+	v1.GET("transactions/:transactionId", cl.TransactionController.GetDetailTransactionController)
+	v1.POST("transactions", cl.TransactionController.CreateTransactionController)
+	v1.DELETE("transactions/:transactionId", cl.TransactionController.DeleteTransactionController)
+	v1.PUT("transactions/:transactionId", cl.TransactionController.UpdateTransactionController)
 }
